d46ff59d59b1d44c070ffc5548b18b95741e44f2
LarissaSL <larissa.silvaedge@gmail.com>
2024-05-26 05:28:56 -0300

    V9.0.1 - Mudanca na ordem das rotas para resolver o bug de entrar em rota errada

--- router/routes.php
+++ router/routes.php
@@ -12,9 +12,9 @@ $routes = [
 
 
     //Rotas Usuario
-    '/usuario/{id}' => 'usuario#UsuarioController#mostrarPerfil',
     '/usuario/login' => 'usuario#UsuarioController#login',
     '/usuario/cadastro' => 'usuario#UsuarioController#cadastrar',
+    '/usuario/{id}' => 'usuario#UsuarioController#mostrarPerfil',
     '/usuario/atualizar/{id}' => 'usuario#UsuarioController#atualizar',
     '/usuario/atualizar/senha/{id}' => 'usuario#UsuarioController#atualizarSenha',
     '/usuario/redefinirSenha/{id}' => 'usuario#UsuarioController#redefinirSenha',
