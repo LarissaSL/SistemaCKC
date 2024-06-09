d8dab6c437ccba2e444a11732884235c1bc2e439
giuliana <gralhagiuliana@gmail.com>
2024-06-09 16:44:47 -0300

    V10.0.1 - atualizacao do copyright - home

--- views/home.php
+++ views/home.php
@@ -7,13 +7,13 @@
     <!-- google fontes -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <!-- ícone de menu  -->
     <link rel="stylesheet" href="/sistemackc/views/Css/variaveis.css">
     <link rel="stylesheet" href="/sistemackc/views/Css/index.css">
 
     <link rel="icon" href="/sistemackc/views/Img/ImgIcones/crash_icon.ico" type="image/x-icon">
-    
+
     <script src="https://unpkg.com/@phosphor-icons/web"></script> <!-- ONDE PEGUEI OS ICON TEMPORARIOS 'phosphor-icons' -->
     <script defer src="/sistemackc/views/Js/nav.js"></script> <!-- O atributo "defer" serve para que o script roda depois do html -->
 
@@ -89,7 +89,7 @@
                     <p class="textoChamada">
                         Então, venha fazer parte dessa família onde a única regra é: <strong>Velocidade, emoção e, é claro, uma pitada de loucura!</strong>
                     </p>
-                    <?php 
+                    <?php
                     if (!isset($_SESSION['tipo']) || $_SESSION['tipo'] != 'Comum') : ?>
                         <div class="botoes">
                             <a class="bt-redirecionar" href="/sistemackc/usuario/login">Entrar</a>
@@ -218,34 +218,47 @@
         <div class="water">
             <svg class="waves" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
                 <defs>
-                    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>
+                    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                 </defs>
                 <g class="parallax">
-                    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(47, 44, 44, 0.7)"/>
-                    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(47, 44, 44, 0.5)"/>
-                    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(49, 46, 46, 0.3)"/>
-                    <use xlink:href="#gentle-wave" x="48" y="7" fill="var(--background-campos)"/>
-                
+                    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(47, 44, 44, 0.7)" />
+                    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(47, 44, 44, 0.5)" />
+                    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(49, 46, 46, 0.3)" />
+                    <use xlink:href="#gentle-wave" x="48" y="7" fill="var(--background-campos)" />
+    
                 </g>
             </svg>
         </div>
         <!-- conteudo na nav -->
         <div class="content">
-      
-            <span class="copyright">© Sistema Gerenciador de corridas de kart. Todos os Direitos Reservados à Manas Code</span>
-     
+            <div class="copyrights">
+                <span class="copyright">© Sistema Gerenciador de corridas de kart. Todos os Direitos Reservados à Manas Code</span>
+                <div class="logos">
+                    <div class="logSistema">
+                        <span class="copySistema">Plataforma</span>
+                        <img class="logo logoSistema" src="/sistemackc/Views/Img/ImgSistema/logoSis_Gerenciador_kart.png" alt="logo do Sistema Gerenciador de Corridas de Kart ">
+                    </div>
+                    <div class="logManas">
+                        <span class="copyDevs">desenvolvedor</span>
+                        <img class="logo logoManasC" src="/sistemackc/Views/Img/ImgSistema/logoManasC.png" alt="logo da desenvolvedora do sistema - Manas Code">
+                    </div>
+                </div>
+
+
+            </div>
+
             <div class="navegation">
                 <div class="contact">
                     <a href="https://www.instagram.com/crashkartchampionship?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">
                         <i class="ph-fill ph-instagram-logo"></i><!-- logo instagram-->
                     </a>
-                    <a href="#" target="_blank">
+                    <a href="https://wa.me/5511984372045" target="_blank">
                         <i class="ph-fill ph-whatsapp-logo"></i><!-- logo whatsapp-->
                     </a>
                 </div>
                 <div class="navigationLink">
                     <a href="/sistemackc/etapas">Etapas</a>
-                    <a href="#">Classificação</a>
+                    <a href="/sistemackc/classificacao">Classificação</a>
                     <a href="/sistemackc/kartodromo">Kartódromos</a>
                 </div>
             </div>
